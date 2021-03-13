f1aac62532877cbaaf3fc7855a2abd7729d18135
Riley Williams <williams.riley@me.com>
2021-03-13 16:39:40 -0500

    more access modifiers

--- Sources/PiholeNetworking/Models/PHStatus.swift
+++ Sources/PiholeNetworking/Models/PHStatus.swift
@@ -7,18 +7,18 @@
 
 import Foundation
 
-open struct PHStatus: Codable {
-	var state:PHState = .unknown
-	var blockedDomainCount:Int = 0
-	var dnsQueryTodayCount:Int = 0
-	var adsBlockedTodayCount:Int = 0
-	var uniqueDomainCount:Int = 0
-	var forwardedQueryCount:Int = 0
-	var cachedQueryCount:Int = 0
-	var totalClientsCount:Int = 0
-	var uniqueClientCount:Int = 0
-	var totalDNSCount:Int = 0
-	var percentAdsToday:Float = 0
+public struct PHStatus: Codable {
+	public var state:PHState = .unknown
+	public var blockedDomainCount:Int = 0
+	public var dnsQueryTodayCount:Int = 0
+	public var adsBlockedTodayCount:Int = 0
+	public var uniqueDomainCount:Int = 0
+	public var forwardedQueryCount:Int = 0
+	public var cachedQueryCount:Int = 0
+	public var totalClientsCount:Int = 0
+	public var uniqueClientCount:Int = 0
+	public var totalDNSCount:Int = 0
+	public var percentAdsToday:Float = 0
 	
 	enum CodingKeys: String, CodingKey {
 		case state = "status"
