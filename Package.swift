16cd3f1b732e021a342153aecced5e6da43ed26e
Riley Williams <williams.riley@me.com>
2021-04-06 22:57:07 -0400

    Bumps minimum watchOS version

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "PiholeNetworking",
 	platforms: [.iOS(.v13),
 				.macOS(.v10_15),
-				.watchOS(.v5),
+				.watchOS(.v6),
 				.tvOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
