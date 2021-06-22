1bea93d76b97a4419da585f0140e5f04bb0cce40
Riley Williams <williams.riley@icloud.com>
2021-06-22 19:52:42 -0400

    tvOS minimum version

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
 	platforms: [.iOS("15"),
 				.macOS("12"),
 				.watchOS("8"),
-				.tvOS("16")],
+				.tvOS("15")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
